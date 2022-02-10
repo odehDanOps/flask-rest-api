8b473d9fee0e792ce94149d74fe00e2512d26ff5
DanOps! <odehdaniel1@gmail.com>
2022-02-10 10:03:39 +0100

    User Resource init

--- App 3-SQLAlchemy/app.py
+++ App 3-SQLAlchemy/app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
-app.secret_key = 'jose'
+app.secret_key = 'danops'
 api = Api(app)
 
 
